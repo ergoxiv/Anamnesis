dc8fcbcaa9f98d724324a8d2966591fa6b32723a
ergoxiv <ergo.ffxiv@gmail.com>
2024-12-20 14:35:30 +0100

    Updated support files & README to reference .NET 9

--- Anamnesis/Install .NET Desktop Runtime.bat
+++ Anamnesis/Install .NET Desktop Runtime.bat
@@ -1,4 +1,4 @@
 @echo off
 title .NET Desktop Runtime Installer
 echo The Microsoft .NET Desktop Runtime will be installed. Please do not close the console until it has completed.
-winget install Microsoft.DotNet.DesktopRuntime.8 -e -v 8.0.10 --accept-package-agreements
+winget install Microsoft.DotNet.DesktopRuntime.9 -e -v 9.0.0 --accept-package-agreements
