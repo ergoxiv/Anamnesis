7bbfccd43ed1d347c95cceffbb58f7c4d82c0f3f
ergoxiv <ergo.ffxiv@gmail.com>
2026-01-24 04:26:50 +0100

    Updated Anamnesis' actor refresh logic

--- Anamnesis/Actor/Refresh/AnamnesisActorRefresher.cs
+++ Anamnesis/Actor/Refresh/AnamnesisActorRefresher.cs
@@ -8,7 +8,6 @@ using Anamnesis.Services;
 using RemoteController.Interop.Delegates;
 using Serilog;
 using System;
-using System.Threading;
 using System.Threading.Tasks;
 
 public enum RedrawStage
@@ -26,10 +25,6 @@ public class AnamnesisActorRefresher : IActorRefresher
 		if (PoseService.Instance.IsEnabled)
 			return false;
 
-		// Ana can't refresh gpose actors
-		//if (actor.IsGPoseActor)
-		//	return false;
-
 		// Ana can't refresh world frozen actors
 		if (PoseService.Instance.FreezeWorldPosition)
 			return false;
@@ -39,28 +34,20 @@ public class AnamnesisActorRefresher : IActorRefresher
 
 	public async Task RefreshActor(ActorMemory actor)
 	{
-		//if (SettingsService.Current.EnableNpcHack && actor.ObjectKind == ActorTypes.Player)
-		//{
-		//	actor.ObjectKind = ActorTypes.EventNpc;
-		//	actor.RenderMode = RenderModes.Unload;
-		//	await Task.Delay(75);
-		//	actor.RenderMode = RenderModes.Draw;
-		//	await Task.Delay(75);
-		//	actor.ObjectKind = ActorTypes.Player;
-		//	actor.RenderMode = RenderModes.Draw;
-		//}
-		//else
-		//{
-		//	actor.RenderMode = RenderModes.Unload;
-		//	await Task.Delay(75);
-		//	actor.RenderMode = RenderModes.Draw;
-		//}
-
 		var handle = ActorService.Instance.ObjectTable.Get<GameObjectMemory>(actor.Address);
-		if (handle == null || !handle.IsValid)
+		if (handle == null)
 			return;
 
-		await this.RedrawService.Redraw(handle);
+		if (SettingsService.Current.EnableNpcHack && actor.ObjectKind == ActorTypes.Player)
+		{
+			actor.ObjectKind = ActorTypes.EventNpc;
+			await this.RedrawService.Redraw(handle);
+			actor.ObjectKind = ActorTypes.Player;
+		}
+		else
+		{
+			await this.RedrawService.Redraw(handle);
+		}
 	}
 }
 
@@ -68,167 +55,48 @@ public class RedrawService
 {
 	private static HookHandle? s_enableDrawHook = null;
 	private static HookHandle? s_disableDrawHook = null;
-	private static HookHandle? s_isReadyToDrawHook = null;
 
 	public RedrawService()
 	{
-		Log.Debug("RedrawService: Registering hooks...");
-		s_enableDrawHook = ControllerService.Instance.RegisterWrapper<GameObject.EnableDraw>();
-		Log.Debug("RedrawService: Registered s_enableDrawHook: {Hook}", s_enableDrawHook);
-		s_disableDrawHook = ControllerService.Instance.RegisterWrapper<GameObject.DisableDraw>();
-		Log.Debug("RedrawService: Registered s_disableDrawHook: {Hook}", s_disableDrawHook);
-		s_isReadyToDrawHook = ControllerService.Instance.RegisterWrapper<GameObject.IsReadyToDraw>();
-		Log.Debug("RedrawService: Registered s_isReadyToDrawHook: {Hook}", s_isReadyToDrawHook);
+		s_enableDrawHook = ControllerService.Instance.RegisterWrapper<Character.EnableDraw>();
+		s_disableDrawHook = ControllerService.Instance.RegisterWrapper<Character.DisableDraw>();
 	}
 
 	public delegate void RedrawEvent(ObjectHandle<GameObjectMemory> obj, RedrawStage stage);
 	public event RedrawEvent? OnRedraw;
 
-	public static async Task DrawWhenReady(int objectIndex)
-	{
-		if (ControllerService.Instance == null || ControllerService.Instance.Framework?.Active != true)
-			return;
-
-		IntPtr currentPtr = ActorService.Instance.ObjectTable.GetAddress(objectIndex);
-		using var obj = new ObjectHandle<GameObjectMemory>(currentPtr, ActorService.Instance.ObjectTable);
-
-		await ControllerService.Instance.Framework.RunOnTickUntilAsync(
-			() => IsReadyToDraw(obj),
-			deferTicks: 5,
-			timeoutTicks: 100,
-			() => EnableDraw(objectIndex));
-	}
-
-	public static bool IsReadyToDraw(ObjectHandle<GameObjectMemory> obj)
-	{
-		if (!obj.IsValid || s_isReadyToDrawHook == null)
-			return false;
-		bool? result = null;
-		try
-		{
-			result = ControllerService.Instance.InvokeHook<bool>(s_isReadyToDrawHook, mode: RemoteController.Interop.DispatchMode.FrameworkTick, args: obj.Address);
-			if (result == null)
-			{
-				Log.Warning("Object is ready to draw invocation returned null");
-			}
-		}
-		catch
-		{
-			Log.Verbose("Failed to invoke 'IsReadyToDraw' hook.");
-		}
-
-		return result ?? false;
-	}
-
-	public static bool DisableDraw(int objectIndex)
-	{
-		IntPtr currentPtr = ActorService.Instance.ObjectTable.GetAddress(objectIndex);
-		using var obj = new ObjectHandle<GameObjectMemory>(currentPtr, ActorService.Instance.ObjectTable);
-
-		if (!obj.IsValid || s_disableDrawHook == null)
-			return false;
-
-		long? result = null;
-		try
-		{
-			result = ControllerService.Instance.InvokeHook<long>(s_disableDrawHook, args: obj.Address, mode: RemoteController.Interop.DispatchMode.FrameworkTick, timeoutMs: 1000);
-			if (result == null)
-			{
-				Log.Warning("Object disable draw invocation returned null");
-			}
-		}
-		catch
-		{
-			Log.Verbose("Failed to invoke 'DisableDraw' hook.");
-		}
-
-		return result.HasValue && result.Value != 0;
-	}
-
-	public static bool EnableDraw(int objectIndex)
-	{
-		IntPtr currentPtr = ActorService.Instance.ObjectTable.GetAddress(objectIndex);
-		using var obj = new ObjectHandle<GameObjectMemory>(currentPtr, ActorService.Instance.ObjectTable);
-
-		if (!obj.IsValid || s_enableDrawHook == null)
-			return false;
-
-		byte? result = null;
-		try
-		{
-			result = ControllerService.Instance.InvokeHook<byte>(s_enableDrawHook, args: obj.Address, mode: RemoteController.Interop.DispatchMode.FrameworkTick, timeoutMs: 1000);
-			if (result == null)
-			{
-				Log.Warning("Object enable draw invocation returned null");
-			}
-		}
-		catch
-		{
-			Log.Verbose("Failed to invoke 'EnableDraw' hook.");
-		}
-
-		return result.HasValue && result.Value != 0;
-	}
-
-	public static async Task WaitForDrawing(int objectIndex)
-	{
-		if (ControllerService.Instance == null || ControllerService.Instance.Framework?.Active != true)
-			return;
-
-		IntPtr currentPtr = ActorService.Instance.ObjectTable.GetAddress(objectIndex);
-		using var obj = new ObjectHandle<GameObjectMemory>(currentPtr, ActorService.Instance.ObjectTable);
-
-		await ControllerService.Instance.Framework.RunOnTickUntilAsync(
-			() => obj.Do(o => o.ModelObject?.IsVisible == true) == true,
-			deferTicks: 5,
-			timeoutTicks: 100);
-	}
-
 	public async Task Redraw(ObjectHandle<GameObjectMemory> obj)
 	{
-		string name = obj.DoRef(a => a.DisplayName) ?? "Unknown";
-		Log.Debug($"Redrawing game object \"{name}\": 0x{obj.Address:X}");
+		if (!obj.IsValid)
+			return;
 
-		ActorService.Instance.ObjectTable.Refresh();
+		string name = obj.DoRef(a => a.DisplayName) ?? "Unknown";
+
 		int objectIndex = ActorService.Instance.ObjectTable.GetIndexOf(obj.Address);
 		if (objectIndex == -1)
 		{
-			Log.Error("Could not find the object index for the actor \"{name}\"");
+			Log.Error($"Could not find the object index for the actor \"{name}\"[Index: {objectIndex}, Address: 0x{obj.Address:X}].");
 			return;
 		}
 
 		try
 		{
-			bool disableResult = DisableDraw(objectIndex);
-			if (!disableResult)
-			{
-				// TODO: Perhaps instead of skipping the redraw, retry?
-				// TOOD: To the same with EnableDraw below.
-				Log.Warning($"Failed to disable draw for object \"{name}\". Skipping redraw.");
-				return;
-			}
-
 			this.OnRedraw?.Invoke(obj, RedrawStage.Before);
 
-			try
+			using (var batch = ControllerService.Instance.CreateBatchInvoke())
 			{
-				await DrawWhenReady(objectIndex);
-			}
-			catch (OperationCanceledException)
-			{
-				Log.Warning("DrawWhenReady timed out. Forcing EnableDraw.");
-				ControllerService.Instance.Framework.RunAfterTicks(3, () => EnableDraw(objectIndex));
-				return;
-			}
+				batch.Mode = RemoteController.Interop.DispatchMode.FrameworkTick;
 
-			await WaitForDrawing(objectIndex);
+				IntPtr objPtr = ActorService.Instance.ObjectTable.GetAddress(objectIndex);
+				batch.AddCall<long>(s_disableDrawHook!, args: objPtr);
+				batch.AddCall<byte>(s_enableDrawHook!, args: objPtr);
+			}
 
 			this.OnRedraw?.Invoke(obj, RedrawStage.After);
-			Log.Debug($"Redrew object \"{name}\".");
 		}
 		catch (Exception ex)
 		{
-			Log.Error(ex, $"Failed to redraw object \"{name}\".");
+			Log.Error(ex, $"Failed to redraw object \"{name}\"[Index: {objectIndex}, Address: 0x{obj.Address:X}].");
 		}
 	}
 }
